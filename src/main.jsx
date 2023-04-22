9c869cd867a95c113872cf66ac3e9a6d5ba4ac3f
Shubham Lolusare <shubhamlolusare@gmail.com>
2023-04-22 21:59:00 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+import ErrorPage from "./pages/error";
+import App from "./pages/app";
+import "./index.css";
+import { loader as profileLoader } from "./pages/profile";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+      <Route index element={<Login />} />
+      <Route path="profile" element={<Profile />} loader={profileLoader} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
